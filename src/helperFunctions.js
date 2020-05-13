0304672019bf4aaf98c83ccbfeba8272791373df
ANN LIN <smilemarg@gmail.com>
2020-05-13 15:38:09 -0400

    allClassObjCreator for get() from firestore

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -66,3 +66,39 @@ export const get2DigitsMinute = (num) => {
   }
   return num + ''
 }
+
+export const classDateCreator = (timeString) => {
+  // receive time zones of all places
+  // convert to user time zone (either 1. convert to UTC then convert to local, or 2. local - original and add up the difference)
+  const localTime = new Date(timeString)
+
+  // get year, month, day to put classes into objects by dates
+  const classDate = new Date()
+  classDate.setFullYear(localTime.getFullYear())
+  classDate.setMonth(localTime.getMonth())
+  classDate.setDate(localTime.getDate())
+
+  return classDate
+}
+
+export const allClassesObjCreator = (snapshot) => {
+  const allClasses = {}
+  snapshot.forEach(doc => {
+    const classDate = classDateCreator(doc.data().time)
+
+    //ORGANIZING CLASS OBJECT
+    //Grab Class Data
+    const classObj = doc.data()
+    // add auto id to id key
+    classObj.id = doc.id
+
+    // allClasses(obj) > classDate(key) > (value)classObj(obj) > {name, price, desc, link, id} 
+    if(allClasses[classDate]){
+      allClasses[classDate].push(classObj)
+    }else{
+      allClasses[classDate] = [classObj]
+    }
+  })
+  console.log(allClasses)
+  return allClasses
+}
--- src/store/actions/generalActions.js
+++ src/store/actions/generalActions.js
@@ -1,4 +1,5 @@
 import db from '../../firebase';
+import { classDateCreator, allClassesObjCreator } from '../../helperFunctions'
 
 //ACTION TYPES
 const DISPLAY_CLASSES_ON_SCHEDULE = 'DISPLAY_CLASSES_ON_SCHEDULE'
@@ -23,38 +24,10 @@ export const displayClassesOnSchedule = (classes) => ({type: DISPLAY_CLASSES_ON_
 
 //THUNK CREATOR
 export const getAllClasses = () => dispatch => {
-  // receive time zones of all places
-  // convert to user time zone (either 1. convert to UTC then convert to local, or 2. local - original and add up the difference)
-  // slice year month date
+  
   db.collection('allClasses').get()
   .then(snapshot => {
-    const allClasses = {}
-    snapshot.forEach(doc => {
-      const timeStamp = doc.data().time
-      console.log('timeStamp', timeStamp)
-      const localTime = new Date(timeStamp)
-      console.log('timeStampAfterDate', localTime)
-
-      // get year, month, day to put classes into objects by dates
-      const classDate = new Date()
-      classDate.setFullYear(localTime.getFullYear())
-      classDate.setMonth(localTime.getMonth())
-      classDate.setDate(localTime.getDate())
-
-      // ORGANIZING CLASS OBJECT
-      // Grab class data
-      const classObj = doc.data()
-      // add auto id to id key
-      classObj.id = doc.id
-
-      // allClasses(obj) > classDate(key) > (value)classObj(obj) > {name, price, desc, link, id} 
-      if(allClasses[classDate]){
-        allClasses[classDate].push(classObj)
-      }else{
-        allClasses[classDate] = [classObj]
-      }
-    })
-    console.log(allClasses)
+    const allClasses = allClassesObjCreator(snapshot)
     dispatch(displayClassesOnSchedule(allClasses))
   })
   .catch(err => {
@@ -65,11 +38,7 @@ export const getAllClasses = () => dispatch => {
 export const getClassesByInstructor = (instructor) => dispatch => {
   db.collection('allClasses').where('teacher', '==', instructor).get()
     .then(snapshot => {
-      const classes = {}
-      snapshot.forEach(doc => {
-        classes[doc.data().time] = doc.data()
-        classes[doc.data().time].id = doc.id
-      })
+      const classes = allClassesObjCreator(snapshot)
       dispatch(displayClassesOnSchedule(classes))
     })
     .catch(err => {
