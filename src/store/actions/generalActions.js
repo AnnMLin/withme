701a1e0889baa54c82f6375564cc7608b7632e3f
ANN LIN <smilemarg@gmail.com>
2020-05-13 15:21:37 -0400

    f/timezone correction

--- src/store/actions/generalActions.js
+++ src/store/actions/generalActions.js
@@ -23,18 +23,35 @@ export const displayClassesOnSchedule = (classes) => ({type: DISPLAY_CLASSES_ON_
 
 //THUNK CREATOR
 export const getAllClasses = () => dispatch => {
+  // receive time zones of all places
+  // convert to user time zone (either 1. convert to UTC then convert to local, or 2. local - original and add up the difference)
+  // slice year month date
   db.collection('allClasses').get()
   .then(snapshot => {
     const allClasses = {}
     snapshot.forEach(doc => {
-      const date = doc.data().time.slice(0, 10)
+      const timeStamp = doc.data().time
+      console.log('timeStamp', timeStamp)
+      const localTime = new Date(timeStamp)
+      console.log('timeStampAfterDate', localTime)
+
+      // get year, month, day to put classes into objects by dates
+      const classDate = new Date()
+      classDate.setFullYear(localTime.getFullYear())
+      classDate.setMonth(localTime.getMonth())
+      classDate.setDate(localTime.getDate())
+
+      // ORGANIZING CLASS OBJECT
+      // Grab class data
       const classObj = doc.data()
+      // add auto id to id key
       classObj.id = doc.id
-      // console.log(classObj)
-      if(allClasses[date]){
-        allClasses[date].push(classObj)
+
+      // allClasses(obj) > classDate(key) > (value)classObj(obj) > {name, price, desc, link, id} 
+      if(allClasses[classDate]){
+        allClasses[classDate].push(classObj)
       }else{
-        allClasses[date] = [classObj]
+        allClasses[classDate] = [classObj]
       }
     })
     console.log(allClasses)
