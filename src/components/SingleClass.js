bca12d9d84b5a18b0a5367a9f134a340420ebc15
ANN LIN <smilemarg@gmail.com>
2020-05-14 00:54:18 -0400

    single class styling

--- src/components/SingleClass.js
+++ src/components/SingleClass.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import allActions from '../store/actions'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 const SingleClass = () => {
   const { classId } = useParams()
@@ -16,6 +16,11 @@ const SingleClass = () => {
 
   return(
     <div id='single-class'>
+      <Link to='/'>
+        <div id='back-arrow-container'>
+          <img id='back-arrow' src={process.env.PUBLIC_URL +  '/icons/arrow-thin.png'} alt=''/>
+        </div>
+      </Link>
       {singleClass.id === classId ?
       <div>
         <div>{singleClass.name}</div>
